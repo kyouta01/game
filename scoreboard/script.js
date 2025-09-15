3df4c01c9ea3d08e9a6914cda7841bcfcfc13689
kyouta01 <105335573+kyouta01@users.noreply.github.com>
2025-09-15 23:44:12 +0700

    Add files via upload

--- scoreboard/script.js
+++ scoreboard/script.js
@@ -33,7 +33,7 @@ resetBtn.addEventListener('click', () => {
 // tampilkan scoreboard saat load
 tampilkanScore();
 
-
+// Tombol Home kembali ke halaman utama
 document.getElementById('homeBtn').addEventListener('click', () => {
-  history.back();   // kembali ke halaman sebelumnya di riwayat browser
+  window.location.href = '/game/materi/index.html'; // ganti dengan nama file halaman utama Anda
 });
